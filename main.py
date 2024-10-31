406cab7aa02494b13c4db2f44738450ce74c9ced
Andrey <maslakan78@gmail.com>
2024-10-31 19:11:39 +0300

    Исправлен баг с отправкой одинаковых писем. Теперь информация берется из полей ввода Entry

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from tkinter import *
 
 
 def send_email():
-    sender_email = 'Denser-m@yandex.ru'
-    recipient_mail = 'denser_m@mail.ru'
-    password = 'eunswfzsnxlvghyo'
-    subject = 'Проверка связи'
-    body = 'Привет из программы!'
+    sender_email = sender_email_entry.get()
+    recipient_mail = recipient_email_entry.get()
+    password = password_entry.get()
+    subject = subject_entry.get()
+    body = body_text.get(1.0, END)
 
     msg = EmailMessage()
     msg.set_content(body)
